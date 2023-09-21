44774597fba3eb3d9e5eef4893941f8d6c813231
keithAwarren <warrenkeith.music@gmail.com>
2023-09-21 14:19:37 -0230

    Re-added JS comments

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
+
+    // Select all elements with class "shoFormButton and close-button"
     const showFormButtons = document.querySelectorAll(".showFormButton");
     const closeButtons = document.querySelectorAll(".close-button");
 
+    // Add click event listener to "showFormButton" elements
     showFormButtons.forEach((button) => {
         button.addEventListener("click", function () {
             const targetId = this.getAttribute("data-target");
@@ -10,6 +13,7 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 
+    // Add click event listener to "close-button" elements
     closeButtons.forEach((button) => {
         button.addEventListener("click", function () {
             const targetModalId = this.getAttribute("data-modal");
@@ -22,13 +26,16 @@ document.addEventListener("DOMContentLoaded", function () {
     const appointmentsForm = document.getElementById("appointmentsForm");
     const contactSelect = document.querySelector("#contactSelect"); 
 
+    // Add submit event listener to contact form
     contactForm.addEventListener("submit", function (e) {
-        e.preventDefault(); 
+        e.preventDefault(); // Prevent form from submitting
 
+        // Get values from contact form
         const name = document.querySelector("#name").value;
         const phone = document.querySelector("#phone").value;
         const email = document.querySelector("#email").value;
 
+        // Create option element to add to contact select 
         const option = document.createElement("option");
 
         option.text = name;
@@ -36,27 +43,35 @@ document.addEventListener("DOMContentLoaded", function () {
         option.setAttribute("data-phone", phone);
         option.setAttribute("data-email", email);
 
+        // Add created option element to select
         contactSelect.appendChild(option);
 
+        // Reset contact form and close modal
         contactForm.reset();
         closeModal("contactsModal");
     });
 
+    // Add submit event listener to appointment form
     appointmentsForm.addEventListener("submit", function (e) {
         e.preventDefault(); 
 
+        // Get selected contact option from contact select
         const selectedOption = contactSelect.options[contactSelect.selectedIndex];
 
+        // Get phone and email data from selected contact
         const selectedPhone = selectedOption.getAttribute("data-phone");
         const selectedEmail = selectedOption.getAttribute("data-email");
 
+        // Get values from the appointment form
         const title = document.querySelector("#title").value;
         const date = document.querySelector("#date").value;
         const time = document.querySelector("#time").value;
 
+        // Create div to display appointment details
         const appointmentInfo = document.createElement("div");
         appointmentInfo.classList.add("appointment-info");
 
+        // Populate appointment info div with appointment details
         appointmentInfo.innerHTML = `
             <p><strong>Contact:</strong> ${selectedOption.textContent}</p>
             <p><strong>Title:</strong> ${title}</p>
@@ -64,13 +79,16 @@ document.addEventListener("DOMContentLoaded", function () {
             <p><strong>Time:</strong> ${time}</p>
         `;
 
+        // Add appointment info to display area
         const appointmentInfoDisplay = document.querySelector("#appointmentInfoDisplay");
         appointmentInfoDisplay.appendChild(appointmentInfo);
 
+        // Reset appointment forms, close modal
         appointmentsForm.reset();
         closeModal("appointmentsModal");
     });
 
+    // Function to close modal by setting display to "none"
     function closeModal(modalId) {
         const modal = document.getElementById(modalId);
         modal.style.display = "none";
